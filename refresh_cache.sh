cfaa58a26a830ffaab3a31fa46f559d9dda1654f
wildabenny <strzelczykster@gmail.com>
2017-06-16 22:08:04 +0200

    removed some directories 2

--- refresh_cache.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-sudo rm -rf ./app/cache/d*
-sudo chmod -R 775 ./app
-sudo chown -R marcin:www-data ./app
-php ./app/console cache:clear -e dev
-sudo chmod -R 775 ./app
-sudo chown -R marcin:www-data ./app
